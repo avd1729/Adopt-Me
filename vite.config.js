c5cad6f4fbf4918e4fce68f794bffa38113e27eb
Aravind <aravind19112003@gmail.com>
2025-06-20 11:23:48 +0530

    modify vite deps to handle _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
   root: "src",
+  plugins: [
+    react(),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
 });
